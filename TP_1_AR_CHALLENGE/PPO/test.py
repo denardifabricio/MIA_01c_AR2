d8bfbc80d2c1acf760d540fcfc7b1305cbf4a869
fabricio.denardi <fabridenardi@gmail.com>
2025-05-27 00:53:40 -0300

    corregir ppo

--- TP_1_AR_CHALLENGE/PPO/test.py
+++ TP_1_AR_CHALLENGE/PPO/test.py
@@ -37,26 +37,9 @@ def generar_video(im_folder, output_path, fps=1):
 
 train_env = make_vec_env(env_id, n_envs=4)
 
-model = PPO(
-    policy='MlpPolicy',
-    env=train_env,
-    learning_rate=0.0005,
-    n_steps=6000,
-    batch_size=128,
-    n_epochs=100,
-    gamma=0.99,
-    gae_lambda=0.95,
-    clip_range=0.2,
-    ent_coef=0.0,
-    vf_coef=0.5,
-    max_grad_norm=0.5,
-    verbose=1,
-    seed=42,
-)
+model = PPO.load(os.path.join(base_path, "result.zip"), env=train_env)
 
-load_path = os.path.join(base_path, "result.zip")
 
-model.load(load_path)
 
 
 
